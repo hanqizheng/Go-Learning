101767572f07f66077bd3100496985c86b04f8a4
hanqizheng <hanqizheng@unitedstack.com>
2018-10-12 11:03:27 +0800

    add http.go

---
+++ exm/http.go
@@ -0,0 +1,78 @@
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+		"fmt"
+		"net/http"
+		"sync"
+		"image"
+    "image/color"
+    "image/gif"
+    "io"
+    "math"
+    "math/rand"
+)
+
+var mu sync.Mutex
+var count int
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+    whiteIndex = 0 // first color in palette
+    blackIndex = 1 // next color in palette
+)
+
+func main() {
+		// 有点像node 的router
+		http.HandleFunc("/", handler)
+		http.HandleFunc("/add", counter)
+		http.HandleFunc("/gif", shower)
+		// 服务器跑起来
+		http.ListenAndServe("localhost:8888", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// 互斥锁
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func shower(w http.ResponseWriter, r *http.Request) {
+	lissajous(w)
+}
+func lissajous(out io.Writer) {
+	const (
+			cycles  = 5     // number of complete x oscillator revolutions
+			res     = 0.001 // angular resolution
+			size    = 400   // image canvas covers [-size..+size]
+			nframes = 64    // number of animation frames
+			delay   = 8     // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+			rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+			img := image.NewPaletted(rect, palette)
+			for t := 0.0; t < cycles*2*math.Pi; t += res {
+					x := math.Sin(t)
+					y := math.Sin(t*freq + phase)
+					img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+							blackIndex)
+			}
+			phase += 0.1
+			anim.Delay = append(anim.Delay, delay)
+			anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
